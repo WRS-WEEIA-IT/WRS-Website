eedc6a7428f38f02a7d693fba1e2bc16009ef830
Olek Golus <olekgolus02@gmail.com>
2023-11-11 14:06:34 +0100

    Add welcome message

--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -7,25 +7,25 @@ const Navbar = () => {
     <div className='flex py-6 px-52 sticky top-0 border-b backdrop-blur-md align-middle gap-4'>
       <nav className='flex space-x-4 lg:space-x-6 items-center'>
         <Link
-          href='/examples/dashboard'
+          href='/'
           className='text-lg font-extrabold transition-colors hover:text-primary'
         >
           WRS WEEIA
         </Link>
         <Link
-          href='/examples/dashboard'
+          href='/info'
           className='text-lg font-medium transition-colors hover:text-primary'
         >
           Dla studentów
         </Link>
         <Link
-          href='/examples/dashboard'
+          href='/about-us'
           className='text-lg font-medium text-muted-foreground transition-colors hover:text-primary'
         >
           O nas
         </Link>
         <Link
-          href='/examples/dashboard'
+          href='/events'
           className='text-lg font-medium text-muted-foreground transition-colors hover:text-primary'
         >
           Nasze wydarzenia
