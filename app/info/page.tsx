a9c07c7574721d087dbd6360738b85ffdf5e57d9
olekgolus11 <olekgolus02@gmail.com>
2023-12-23 22:11:29 +0100

    Highlight current path on navbar

--- app/info/page.tsx
+++ app/info/page.tsx
@@ -1,44 +1,7 @@
-import InfoCard from '@/components/info-card/InfoCard';
-import { Book, CalendarCheck, DoorOpen, GraduationCap, Lightbulb, ShipWheel } from 'lucide-react';
+import InfoSection from './[sections]/InfoSection';
 
 const Info = () => {
-    return (
-        <div className='site-container py-12'>
-            <div className=' grid grid-cols-3 gap-4 grid-flow-dense'>
-                <InfoCard
-                    title='Savoir vivre'
-                    description='Savoir vivre studenta, czyli jak zwracać się do władz uczelni'
-                    color='blue'
-                    Icon={Book}
-                />
-                <InfoCard title='Koła naukowe' description='Koła naukowe działające na naszym wydziale' color='green' Icon={ShipWheel} />
-                <InfoCard
-                    title='Kierunki'
-                    description='Kierunki studiów na naszym wydziale wraz z opisami specjalizacji'
-                    color='red'
-                    Icon={DoorOpen}
-                />
-                <InfoCard
-                    title='Studia magisterskie'
-                    description='Kierunki studiów na studiach drugiego stopnia na naszym wydziale'
-                    color='yellow'
-                    Icon={GraduationCap}
-                />
-                <InfoCard
-                    title='Wydarzenia'
-                    description='Najważniejsze wydarzenia na naszym wydziale'
-                    color='purple'
-                    Icon={CalendarCheck}
-                />
-                <InfoCard
-                    title='O nas, o WRS'
-                    description='Trochę o nas - o WRS, oraz o tym jak do nas dołączyć'
-                    color='pink'
-                    Icon={Lightbulb}
-                />
-            </div>
-        </div>
-    );
+    return <InfoSection />;
 };
 
 export default Info;
--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -1,32 +1,52 @@
+'use client';
+
 import Link from 'next/link';
 import { ModeToggle } from '../ui/modeToggle';
 import SearchBar from '../search-bar/SearchBar';
+import { usePathname } from 'next/navigation';
 
 const Navbar = () => {
-  return (
-    <div className='flex py-6 site-container sticky top-0 border-b backdrop-blur-md align-middle gap-4 z-30'>
-      <nav className='flex gap-4 lg:gap-6 items-center whitespace-nowrap text-sm md:text-sm lg:text-base'>
-        <Link href='/' className='font-extrabold transition-colors text-hover'>
-          WRS WEEIA
-        </Link>
-        <div className='hidden md:flex md:gap-4 lg:gap-6 '>
-          <Link href='/info' className='font-medium text-muted-foreground transition-colors text-hover'>
-            Dla studentów
-          </Link>
-          <Link href='/about-us' className='font-medium text-muted-foreground transition-colors text-hover'>
-            O nas
-          </Link>
-          <Link href='/events' className='font-medium text-muted-foreground transition-colors text-hover'>
-            Nasze wydarzenia
-          </Link>
+    const currentPath = usePathname();
+
+    return (
+        <div className='flex py-6 site-container sticky top-0 border-b backdrop-blur-md align-middle gap-4 z-30'>
+            <nav className='flex gap-4 lg:gap-6 items-center whitespace-nowrap text-sm md:text-sm lg:text-base'>
+                <Link href='/' className='font-extrabold transition-colors text-hover'>
+                    WRS WEEIA
+                </Link>
+                <div className='hidden md:flex md:gap-4 lg:gap-6 '>
+                    <Link
+                        href='/info'
+                        className={`font-medium text-muted-foreground transition-colors text-hover ${
+                            currentPath === '/info' ? 'text-primary' : ''
+                        }`}
+                    >
+                        Dla studentów
+                    </Link>
+                    <Link
+                        href='/about-us'
+                        className={`font-medium text-muted-foreground transition-colors text-hover ${
+                            currentPath === '/about-us' ? 'text-primary' : ''
+                        }`}
+                    >
+                        O nas
+                    </Link>
+                    <Link
+                        href='/events'
+                        className={`font-medium text-muted-foreground transition-colors text-hover ${
+                            currentPath === '/events' ? 'text-primary' : ''
+                        }`}
+                    >
+                        Nasze wydarzenia
+                    </Link>
+                </div>
+            </nav>
+            <div className='ml-auto'>
+                <SearchBar />
+            </div>
+            <ModeToggle />
         </div>
-      </nav>
-      <div className='ml-auto'>
-        <SearchBar />
-      </div>
-      <ModeToggle />
-    </div>
-  );
+    );
 };
 
 export default Navbar;
