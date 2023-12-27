21eb1740441dd27991459e64310ebf4dce78b4e6
olekgolus11 <olekgolus02@gmail.com>
2023-12-27 23:20:37 +0100

    Add info paragraphs to info site

--- app/info/[sections]/InfoSection.tsx
+++ app/info/[sections]/InfoSection.tsx
@@ -4,6 +4,16 @@ import { infoCardRoutes } from '@/lib/routes/routes';
 const InfoSection = () => {
     return (
         <div className='site-container py-12'>
+            <div className='site-container text-center'>
+                <p className='text-2xl font-semibold mb-3'>Najważniejsze informacje</p>
+                <p className='text-muted-foreground mb-2 text-xs md:text-sm'>
+                    Nie wiesz jak napisać maila do dziekana? A może nawet nie wiesz kto to jest dziekan?
+                    <br /> Lub nawet nie zdążyłeś jeszcze się zapisać na studia?
+                </p>
+                <p className='bg-[size:200%] animate-background-pulse whitespace-nowrap mb-12 text-sm md:text-base bg-gradient-to-r from-violet-500 to-pink-500 text-transparent bg-clip-text'>
+                    Nie martw się, wszystko znajdziesz tutaj! 👇
+                </p>
+            </div>
             <div className='grid grid-cols-2 md:grid-cols-3 gap-4 auto-rows-max'>
                 {infoCardRoutes.map((route) => (
                     <InfoCard
