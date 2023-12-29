57cf5cbc3bafe77f23050fa2a689b92525b0b45a
olekgolus11 <olekgolus02@gmail.com>
2023-12-29 16:37:37 +0100

    Fix facebook shrink icon

--- components/footer/Footer.tsx
+++ components/footer/Footer.tsx
@@ -1,24 +1,28 @@
 import { Facebook, Mail, Phone } from 'lucide-react';
 
 const Footer = () => {
-  return (
-    <footer className='footer site-container py-12 border-t flex flex-col gap-4'>
-      <div className='flex gap-4 text-muted-foreground'>
-        <Mail />
-        <p>eeia@samorzad.p.lodz.pl</p>
-      </div>
-      <div className='flex gap-4 text-muted-foreground'>
-        <Phone />
-        <p>(+48) 123 123 123</p>
-      </div>
-      <a href='https://www.facebook.com/weeia'>
-        <div className='flex gap-4 text-muted-foreground'>
-          <Facebook />
-          <p>Wydział Elektrotechniki Elektroniki Informatyki i Automatyki</p>
-        </div>
-      </a>
-    </footer>
-  );
+    return (
+        <footer className='footer site-container py-12 border-t flex flex-col gap-4'>
+            <div className='flex gap-4 text-muted-foreground'>
+                <div className='w-[24px] h-[24px]'>
+                    <Mail />
+                </div>
+                <p>eeia@samorzad.p.lodz.pl</p>
+            </div>
+            <div className='flex gap-4 text-muted-foreground'>
+                <div className='w-[24px] h-[24px]'>
+                    <Phone />
+                </div>
+                <p>(+48) 123 123 123</p>
+            </div>
+            <a href='https://www.facebook.com/weeia' className='flex gap-4 text-muted-foreground '>
+                <div className='w-[24px] h-[24px]'>
+                    <Facebook />
+                </div>
+                <p>Wydział Elektrotechniki Elektroniki Informatyki i Automatyki</p>
+            </a>
+        </footer>
+    );
 };
 
 export default Footer;
