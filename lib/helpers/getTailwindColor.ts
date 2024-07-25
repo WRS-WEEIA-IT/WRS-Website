a90d32f72823602ab430fe3d5285ace51b1a19a0
olekgolus11 <olekgolus02@gmail.com>
2024-07-25 17:30:57 +0200

    Finish courses page

--- lib/helpers/getTailwindColor.ts
+++
@@ -1,5 +0,0 @@
-const getTailwindColor = (color: string, strength: string) => {
-    return `bg-${color}-${strength} hover:bg-${color}-${(parseInt(strength) + 100).toString()}`;
-};
-
-export default getTailwindColor;
