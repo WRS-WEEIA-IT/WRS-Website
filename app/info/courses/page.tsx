a80b36dcad7f7d17e28a5850bc7e66bad06f229d
Olek Golus <olekgolus02@gmail.com>
2024-08-01 17:22:02 +0200

    Update courses section

--- app/info/courses/page.tsx
+++ app/info/courses/page.tsx
@@ -1,24 +1,11 @@
-import { collection, getDocs } from 'firebase/firestore/lite';
-import { appDb } from '@/lib/config/firebase';
-import Course from '@/lib/interfaces/firebase/Course';
-import CourseCard from '@/components/course-card/CourseCard';
-
-const getCourses = async (): Promise<Course[]> => {
-    const coursesCollection = collection(appDb, 'courses');
-    const coursesSnapshot = await getDocs(coursesCollection);
-    const coursesList = coursesSnapshot.docs.map((doc) => doc.data());
-    return coursesList as Course[];
-};
+import CoursesGridSection from './[sections]/CoursesGridSection';
+import FirstDegreeHeaderSection from './[sections]/FirstDegreeHeaderSection';
 
 const CoursesPage = async () => {
-    const courses: Course[] = await getCourses();
-    courses.sort((a, b) => a.title.localeCompare(b.title));
-
     return (
-        <div className='site-container grid grid-cols-1 lg:grid-cols-2 gap-4 my-12'>
-            {courses.map((course) => (
-                <CourseCard key={course.title} {...course} />
-            ))}
+        <div>
+            <FirstDegreeHeaderSection />
+            <CoursesGridSection />
         </div>
     );
 };
