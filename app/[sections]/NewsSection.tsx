b742fb98566470c4df2c9565ec0bcec2cd6ecc0b
olekgolus11 <olekgolus02@gmail.com>
2024-08-12 20:15:31 +0200

    Add news page

--- app/[sections]/NewsSection.tsx
+++ app/[sections]/NewsSection.tsx
@@ -2,7 +2,7 @@ import NewsFeed from '@/components/news-feed/NewsFeed';
 import { getFacebookPosts } from '@/lib/utils';
 
 const NewsSection = async () => {
-    const { posts } = await getFacebookPosts(null, 3);
+    const { posts } = await getFacebookPosts(null, 2);
 
     return (
         <section id='news' className='flex flex-col gap-6 site-container'>
