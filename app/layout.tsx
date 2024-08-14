69dbb1b230d9ba3b75ec485e6299a3b958a42adc
olekgolus11 <olekgolus02@gmail.com>
2024-08-14 22:49:43 +0200

    Add basic SEO

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,17 +6,49 @@ import { GeistSans } from 'geist/font/sans';
 import Footer from '@/components/footer/Footer';
 
 export const metadata: Metadata = {
-    title: 'WRS EEIA',
-    description: 'Oficjalna strona WRS EEIA Politechniki Łódzkiej',
-    keywords: 'WRS, EEIA, Politechnika Łódzka, Łódź, studia, studenci, studenckie, samorząd, samorząd studencki',
-    applicationName: 'Strona WRS EEIA',
+    title: 'WRS WEEIA',
+    description: 'Oficjalna strona WRS WEEIA Politechniki Łódzkiej',
+    keywords: [
+        'WRS',
+        'EEIA',
+        'WEEIA',
+        'WRS WEEIA',
+        'Politechnika Łódzka',
+        'Łódź',
+        'studia',
+        'studenci',
+        'studenckie',
+        'samorząd',
+        'samorząd studencki',
+        'samorząd studencki WEEIA',
+        'strona WRS WEEIA',
+    ],
+    applicationName: 'Strona WRS WEEIA',
+    metadataBase: new URL('https://wrs-weeia.vercel.app'),
+    openGraph: {
+        title: 'Oficjalna strona WRS WEEIA',
+        description: 'Oficjalna strona WRS WEEIA Politechniki Łódzkiej',
+        type: 'website',
+        locale: 'pl_PL',
+        siteName: 'Strona WRS WEEIA',
+        url: 'https://wrs-weeia.vercel.app',
+        images: [
+            {
+                url: 'https://wrs-weeia.vercel.app/logo_WEEIA_WRS.jpg',
+                width: 510,
+                height: 330,
+                alt: 'WRS WEEIA',
+            },
+        ],
+    },
+    manifest: '/manifest.json',
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
     return (
         <html lang='en'>
             <body className={GeistSans.className}>
-                <ThemeProvider attribute='class' defaultTheme='dark' enableSystem disableTransitionOnChange>
+                <ThemeProvider attribute='class' defaultTheme='light' enableSystem disableTransitionOnChange>
                     <Navbar />
                     {children}
                     <Footer />
