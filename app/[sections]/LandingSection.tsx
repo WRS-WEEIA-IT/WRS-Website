59bb801ba39c83bd0c98c164d6bb6ac384af858b
olekgolus11 <olekgolus02@gmail.com>
2024-08-14 19:28:00 +0200

    Change main text

--- app/[sections]/LandingSection.tsx
+++ app/[sections]/LandingSection.tsx
@@ -5,11 +5,12 @@ const LandingSection = () => {
                 id='landing-text'
                 className='text-background dark:text-foreground flex flex-col items-center justify-between gap-3 md:gap-6 site-container text-center'
             >
-                <p className='text-2xl md:text-4xl lg:text-5xl xl:text-5xl'>Połączeni studiowaniem.</p>
-                <p className='text-3xl md:text-5xl lg:text-6xl xl:text-7xl'>
-                    <span className='hidden md:inline-block'>💪 </span>
-                    {' Rozwijani'} <span className=' text-yellow-300 drop-shadow-[0_0_45px_rgba(200,200,0,1)]'>WRS</span>
-                    <span className='whitespace-nowrap'>em 💪</span>
+                <p className='text-2xl md:text-4xl lg:text-5xl xl:text-5xl'>There is no WEEIA</p>
+                <p className='text-2xl md:text-4xl lg:text-5xl xl:text-5xl'>without</p>
+                <p className='text-4xl md:text-6xl lg:text-7xl xl:text-8xl'>
+                    <span className='whitespace-nowrap'>💪 </span>
+                    <span className='text-yellow-300 drop-shadow-[0_0_45px_rgba(200,200,0,1)]'>WE</span>
+                    <span className='whitespace-nowrap'> 💪</span>
                 </p>
             </section>
         </div>
